48f75c5efa3cfc1ddc5efded92583da973d93ff6
Sourabh Bagde <sourabh.bagde.1.sb@gmail.com>
2025-04-24 19:25:23 -0500

    Added browsersList, and scripts.

--- App.js
+++ App.js
@@ -1,15 +1,2 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-const parent = React.createElement(
-  'div',
-  { id: 'parent' },
-  React.createElement('div', { id: 'child' }, [
-    React.createElement('h1', { id: 'heading1' }, 'This is h1 tag'),
-    React.createElement('h2', { id: 'heading2' }, 'This is h2 tag'),
-  ]),
-);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(parent);
