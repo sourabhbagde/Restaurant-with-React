f42e71734fa299827410511f8435dacb72a666e0
Sourabh Bagde <sourabh.bagde.1.sb@gmail.com>
2025-05-05 17:22:33 -0500

    Added hooks useState() to filter rating of restaurants.

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
-
+import { useState } from 'react';
 import restaurantList from '../utils/mockData';
 import RestaurantCard from './RestaurantCard';
 
 const Body = () => {
+  const [restaurantListFiltered, setFilteredListOfRestaurant] =
+    useState(restaurantList);
   return (
     <div className="body">
-      <div className="search-bar">Search Bar</div>
+      {/* <div className="search-bar">Search Bar</div> */}
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            console.log('Button Clicked');
+
+            const filteredList = restaurantList.filter(
+              (res) => res.data.rating >= 4.5,
+            );
+            setFilteredListOfRestaurant(filteredList);
+            console.log(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="restaurant-container">
-        {restaurantList.map((restaurant) => (
+        {restaurantListFiltered.map((restaurant) => (
           <RestaurantCard
             key={restaurant.data.id}
             restaurantData={restaurant}
