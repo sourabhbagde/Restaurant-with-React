7c0c573bb73f820954e8297b229cc094bfcaaf1f
Sourabh Bagde <sourabh.bagde.1.sb@gmail.com>
2025-05-12 16:07:02 -0500

    Shimmer UI is added

---
+++ src/components/Shimmer.jsx
@@ -0,0 +1,25 @@
+const Shimmer = () => {
+  return (
+    <div className="shimmer-cotainer">
+      <div className="shimmer-card"> card </div>
+      <div className="shimmer-card"> card </div>
+      <div className="shimmer-card"> card </div>
+      <div className="shimmer-card"> card </div>
+      <div className="shimmer-card"> card </div>
+      <div className="shimmer-card"> card </div>
+      <div className="shimmer-card"> card </div>
+      <div className="shimmer-card"> card </div>
+      <div className="shimmer-card"> card </div>
+      <div className="shimmer-card"> card </div>
+      <div className="shimmer-card"> card </div>
+      <div className="shimmer-card"> card </div>
+      <div className="shimmer-card"> card </div>
+      <div className="shimmer-card"> card </div>
+      <div className="shimmer-card"> card </div>
+      <div className="shimmer-card"> card </div>
+      <div className="shimmer-card"> card </div>
+    </div>
+  );
+};
+
+export default Shimmer;
