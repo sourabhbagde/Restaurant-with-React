4a033a17c66b36d81d8ac26a253b04ed5edaf590
Sourabh Bagde <sourabh.bagde.1.sb@gmail.com>
2025-05-17 20:33:28 -0500

    Contact Us Form is drafted.

--- src/components/ContactUsCard.jsx
+++ src/components/ContactUsCard.jsx
@@ -10,6 +10,29 @@ const ContactUsCard = () => {
           Email Us at <span className="email">{EMAIL}</span>
         </h3>
       </div>
+      <div className="contact-us-card">
+        <h2> Leave Us a Note</h2>
+        <input
+          type="text"
+          id="name"
+          className="contact-info"
+          placeholder="Full Name"
+        ></input>
+        <input
+          type="email"
+          id="email"
+          className="contact-info"
+          placeholder="Email Address (ex: example@abc.com)"
+        ></input>
+
+        <textarea
+          type="text"
+          id="text"
+          className="contact-info-message"
+          placeholder="Enter Message"
+        ></textarea>
+        <input type="submit" className="submit-btn" value="Submit" />
+      </div>
     </div>
   );
 };
