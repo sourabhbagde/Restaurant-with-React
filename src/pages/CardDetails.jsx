ec664628f4a8afa9f36e48ada5d8a1fc8b6a1d01
Sourabh Bagde <sourabh.bagde.1.sb@gmail.com>
2025-05-23 04:40:19 -0500

    individual restaurant card is added.

---
+++ src/pages/CardDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+import SpecificRestaurantCard from '../components/SpecificRestaurantCard';
+const CardDetails = () => {
+  return (
+    <div>
+      <Header />
+      <SpecificRestaurantCard />
+    </div>
+  );
+};
+
+export default CardDetails;
