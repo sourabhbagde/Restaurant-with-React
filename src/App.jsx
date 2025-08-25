00a626fd4937926ab10c6c3141cd15686a065684
Sourabh Bagde <sourabh.bagde.1.sb@gmail.com>
2025-08-25 15:55:04 -0500

    Refactored Pages...

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { Children } from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router';
 import AboutUs from './pages/AboutUs';
-import Home from './pages/Home';
 import ContactUs from './pages/ContactUs';
 import Cart from './pages/Cart';
 import Error from './components/Error';
+import Header from './components/Header';
+import Body from './components/Body';
 
 const AppLayOut = () => {
   return (
     <div className="app">
-      <Home />
+      <Header />
+      {/* <Outlet /> // for rendering children routes inside the parent route. */}
+      <Outlet />
     </div>
   );
 };
@@ -18,19 +21,26 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayOut />,
+    // here paths for other components are used as children routes using Outlet component.
+    children: [
+      {
+        path: '/',
+        element: <Body />,
+      },
+      {
+        path: '/about-us',
+        element: <AboutUs />,
+      },
+      {
+        path: '/contact-us',
+        element: <ContactUs />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+    ],
     errorElement: <Error />,
-  },
-  {
-    path: '/about-us',
-    element: <AboutUs />,
-  },
-  {
-    path: '/contact-us',
-    element: <ContactUs />,
-  },
-  {
-    path: '/cart',
-    element: <Cart />,
   },
 ]);
 
