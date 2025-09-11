2567c045b622884210d0788c1bd820f8331b3def
Sourabh Bagde <sourabh.bagde.1.sb@gmail.com>
2025-09-11 16:04:39 -0500

    Restaurant cards are linked to the respective restaurant info and able to open restaurant page.

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import restaurantList from '../utils/restaurantListData';
 import { useParams } from 'react-router-dom';
 const RestaurantMenu = () => {
-  // useEffect(() => {}, []);
+  const [restaurantInfo, setRestaurantInfo] = useState(null);
   const { resId: id } = useParams(); // taken the restaurant ID from the URL as params.
-  const restaurant = restaurantList.find((res) => res.data.id === id)?.data; // finding the restaurant by ID.
-  if (!restaurant) {
-    return <h1>Restaurant with ID {id} not found</h1>; // if restaurant id is not found.
-  }
+  useEffect(() => {
+    const restaurant = restaurantList.find((res) => res.data.id === id)?.data;
+    setRestaurantInfo(restaurant || null); // if restaurant not found, set to null such that it doesn't make it undefined.
+  }, [id]); // on change of id (as dependency) re-fetch / re-renders the restaurant info
+
+  if (restaurantInfo === null) return <h2>Restaurant not found</h2>;
+
+  const { name, cuisines, rating, deliveryTime } = restaurantInfo || {}; // destructuring the restaurant info object to get the required details.
+
   return (
     <div className="restaurant-menu">
-      <h1>{restaurant.name}</h1>
-      <img src={restaurant.restaurantImage} alt={restaurant.name} />
-      <h2>{restaurant.name} Menu</h2>
-      <h4>{restaurant.cuisines.join(', ')}</h4>
+      <h1>{name}</h1>
+      <h3>Rating 🌟 {rating} stars</h3>
+      <h3>Delivery Time: {deliveryTime} mins</h3>
+      <h2>Menu</h2>
+      <h4>{cuisines.join(', ')}</h4>
     </div>
   );
 };
