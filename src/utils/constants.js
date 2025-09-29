21949b0d960f3a3e295978b66fe42ccb06a3f349
Sourabh Bagde <sourabh.bagde.1.sb@gmail.com>
2025-09-28 20:43:01 -0500

    Hard coded live status - changed to constants.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,5 @@ export const EXAMPLE = 1;
 export const EXAMPLE2 = 2;
 export const EMAIL = 'oujisavi@gmail.com';
 export const RESTAURANTNAME = 'RESTAURANT APP';
+export const ISLIVE = 'live 🟢';
+export const ISOFFLINE = 'offline 🔴';
